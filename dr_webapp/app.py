70422bcf94be8be050c22fa4aeafac0fb38e6f34
logeshlogu221-cell <logeshlogu221@gmail.com>
2026-04-20 12:07:36 +0530

    Fix port for Render deployment

--- dr_webapp/app.py
+++ dr_webapp/app.py
@@ -223,5 +223,9 @@ def predict():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+#if __name__ == "__main__":
+   # app.run(debug=True, port=5000)
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
